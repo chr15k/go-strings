3cc2f3f9f549e8a61f30e74138744cc9db79b14c
Chris Keller <ctk8501@gmail.com>
2024-10-27 19:51:56 +0000

    main | update

--- main.go
+++ main.go
@@ -1,20 +1,14 @@
-# Go Strings
-
-Some useful string manipulation functions, extending the standard `strings` Go library.
-
-## Usage
-
-```go
 package main
 
 import (
 	"fmt"
+
 	"github.com/chr15k/go-strings/str"
 )
 
 func main() {
+
 	mask := str.Mask("chris@example.com", "*", 3, 8)
 
 	fmt.Println(mask) // chr********le.com
 }
-```
